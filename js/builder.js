a1c8501af59036b60c965109a1762833348c4ad8
shana43 <stqbrry@gmail.com>
2026-07-27 20:44:35 +0530

    create project structure and home page

---
+++ js/builder.js
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Builder</title>
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<header>
+    <h1>DevFolio</h1>
+
+    <nav>
+        <a href="../index.html">Home</a>
+        <a href="builder.html">Portfolio Builder</a>
+    </nav>
+</header>
+
+<main>
+    <h2>Portfolio Builder</h2>
+
+    <form>
+        <label>Full Name</label><br>
+        <input type="text" placeholder="dharshana"><br><br>
+
+        <label>Profession</label><br>
+        <input type="text" placeholder="Software Developer"><br><br>
+
+        <label>Email</label><br>
+        <input type="email" placeholder="stqbrry@gmail.com"><br><br>
+
+        <label>About Me</label><br>
+        <textarea rows="5" placeholder="i am a btech student passionate about web development. i have experience in html, css, and javascript."></textarea><br><br>
+
+        <label>Skills</label><br>
+        <input type="text" placeholder="HTML, CSS, JavaScript"><br><br>
+
+        <label>Projects</label><br>
+        <textarea rows="4" placeholder="Currently working on my first web development project, DevFolio – Portfolio Builder. Looking forward to building more projects and expanding my portfolio."></textarea><br><br>
+
+        <label>GitHub Profile</label><br>
+        <input type="url" placeholder="https://github.com/shana43"><br><br>
+
+        <label>LinkedIn Profile</label><br>
+        <input type="url" placeholder="https://linkedin.com/in/dharshana-j"><br><br>
+
+        <button type="submit">Generate Portfolio</button>
+    </form>
+
+</main>
+
+<footer>
+    <p>© 2026 DevFolio. All Rights Reserved.</p>
+</footer>
+
+<script src="../js/builder.js"></script>
+
+</body>
+</html>
