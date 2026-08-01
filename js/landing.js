f15ce5a39e73155411f946675da62c8abb06a1b0
shana43 <stqbrry@gmail.com>
2026-08-01 21:47:39 +0530

    update changes

---
+++ js/landing.js
@@ -0,0 +1,47 @@
+// ==============================
+// Typing Animation
+// ==============================
+
+const text =
+"AI & Data Science Student • Web Developer • Lifelong Learner";
+
+let index = 0;
+
+const typing = document.getElementById("typing");
+
+function typeEffect(){
+
+    if(index < text.length){
+
+        typing.innerHTML += text.charAt(index);
+
+        index++;
+
+        setTimeout(typeEffect,60);
+
+    }
+
+}
+
+window.onload = typeEffect;
+
+
+// ==============================
+// Enter Portfolio Button
+// ==============================
+
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click",()=>{
+
+    document.body.style.transition="opacity .8s";
+
+    document.body.style.opacity="0";
+
+    setTimeout(()=>{
+
+        window.location.href="portfolio.html";
+
+    },800);
+
+});
